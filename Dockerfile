83ad2fca04927fe4c022fafcfa5e6e28f179976f
luis <luiscarvajal2693@gmail.com>
2018-06-04 18:27:20 -0500

    main changes in the page

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM nginx:alpine
+
+# docker run -d --rm -p 80:80 -v c:/sources/prettyprincess:/usr/share/nginx/html/ nginx:alpine
+
+EXPOSE 80
--- js/jqueryslidemenu.js
+++ js/jqueryslidemenu.js
@@ -7,7 +7,7 @@
 //Update: April 12th, 10: Fixed compat issue with jquery 1.4x
 
 //Specify full URL to down and right arrow images (23 is padding-right to add to top level LIs with drop downs):
-var arrowimages={down:['downarrowclass', 'images/down.jpg', 23], right:['rightarrowclass', 'images/right.jpg']}
+var arrowimages={down:['downarrowclass', '', 23], right:['rightarrowclass', 'images/right.jpg']}
 
 var jqueryslidemenu={
 
